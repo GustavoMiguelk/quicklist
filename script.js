536bf197c265ec87d4d4f1ff3df471d61b321d7a
GustavoMiguelk <miguelgustavo004@gmail.com>
2025-03-18 14:12:07 -0300

    Verificação do Input

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const items = document.querySelector("ul")
 const newItem = document.getElementById("item")
 const form = document.querySelector("form")
 
-// Resgatando o item a ser adicionado
-form.addEventListener("submit", (event) => {
+    // Resgatando o item a ser adicionado
+    form.addEventListener("submit", (event) => {
     event.preventDefault()
+
+    // Verificando se os inputs contém algo
+    if(newItem.value.trim() === ""){
+        alert("Erro! Insira um valor válido.")
+        return
+    }
 
     // Criando os novos Elementos
     const newListItem = document.createElement("li")
@@ -18,6 +24,6 @@ form.addEventListener("submit", (event) => {
     newListItem.append(checkItem, newItem.value)
     items.prepend(newListItem)
 
+    // Redefinindo o valor do Input
     newItem.value = ""
-
 })
